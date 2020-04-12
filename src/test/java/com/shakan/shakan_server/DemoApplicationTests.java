0358f4f6c998d279f54bce6dadffe343940b2431
takechann <kurotake1130@gmail.com>
2020-04-13 02:08:08 +0900

    Spring Initializrより、shakanアプリのひな型を作成

---
+++ src/test/java/com/shakan/shakan_server/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shakan.shakan_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
